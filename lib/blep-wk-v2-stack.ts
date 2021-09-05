ccc990a12f6aeb7002a68a119700fb91416f5fbe
crwebb85 <51029315+crwebb85@users.noreply.github.com>
2021-09-05 13:41:25 -0400

    Added ESLint and Prettier (#2)
    Added ESLint and Prettier.

--- lib/blep-wk-v2-stack.ts
+++ lib/blep-wk-v2-stack.ts
@@ -3,6 +3,5 @@ import * as cdk from 'monocdk';
 export class BlepWkV2Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
   }
 }
