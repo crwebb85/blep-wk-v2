8f46bb5c99a8775ff1e682e57f5902cb9465fa3d
crwebb85 <crwebb85@gmail.com>
2021-09-05 17:03:19 -0400

    Fixed the lambda.

--- lib/blep-wk-v2-stack.ts
+++ lib/blep-wk-v2-stack.ts
@@ -1,15 +1,19 @@
 import * as cdk from 'monocdk';
 import { LambdaRestApi } from 'monocdk/lib/aws-apigateway';
-import { Code, Runtime, Function } from 'monocdk/lib/aws-lambda';
+import { Runtime } from 'monocdk/lib/aws-lambda';
+import { NodejsFunction } from 'monocdk/lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class BlepWkV2Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const apiLambda = new Function(this, 'Lambda-API', {
+    const apiLambda = new NodejsFunction(this, 'Lambda-API', {
       runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset('lambda'), // code loaded from "lambda" directory
-      handler: 'index.handler',
+      memorySize: 128,
+      entry: path.join(__dirname, '../lambda/index.ts'),
+      handler: 'handler',
+      logRetention: 90,
     });
 
     new LambdaRestApi(this, 'API-Main', {
